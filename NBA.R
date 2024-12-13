735a1acc97f017e3afc3fa839786b4bbd06b7463
jeffyue93 <jby5373@psu.edu>
2024-12-13 15:49:56 -0500

    Update NBA.R

--- NBA.R
+++ NBA.R
@@ -5,12 +5,7 @@ library(ggplot2)
 NBAplayers = read_sheet("https://docs.google.com/spreadsheets/d/1eJ5BV-S4vy0DPsrENh_L-Q9VYgwmnTQiOeSHWawqNhc/edit?gid=0#gid=0")
 NBAteams = read_sheet("https://docs.google.com/spreadsheets/d/1lHVKe28VY8DfM-UaQNchnHNAeOHTo9jSMybWMEoEVJk/edit?gid=0#gid=0")
 View(NBAplayers)
-NBAplayers2 = NBAplayers[-c(1,6,7,8,9,10,11,12,13,15,16,17,18,19,20,22,23,24,25,26,27,28,29,30)]
-NBAplayers2 = na.omit(NBAplayers2)
-View(NBAplayers2)
-FTPercent = NBAplayers2 %>% group_by(Player) %>% 
-  summarise(FTPer = mean(NBAplayers2[c(6)]))
-NBAplayers3 = NBAplayers[-c(1,4,5,9,10,11,12,13,14,15,16,17,18,19,20,21,24)]
+NBAplayers3 = NBAplayers[-c(1,4,5,9,10,11,12,13,14,15,16,17,18,19,20,21,24,29)]
 View(NBAplayers3)
 Ageppg = NBAplayers3 %>% group_by(NBAplayers3[c(2)]) %>%
   summarise(Avgppg = mean(PTS))
@@ -28,10 +23,9 @@ Ageblk = NBAplayers3 %>% group_by(NBAplayers3[c(2)]) %>%
   summarise(Avgblk = mean(BLK))
 Agetov = NBAplayers3 %>% group_by(NBAplayers3[c(2)]) %>%
   summarise(Avgtov = mean(TOV))
-Agepf = NBAplayers3 %>% group_by(NBAplayers3[c(2)]) %>%
-  summarise(Avgpf = mean(PF))
-Agepts = NBAplayers3 %>% group_by(NBAplayers3[c(2)]) %>%
-  summarise(Avgpts = mean(PTS))
 
-Agestat = data.frame()
+Agestat = data.frame(Ageppg,Ageast[-c(1)],
+Agemp[-c(1)],Ageorb[-c(1)],Agedrb[-c(1)],Agestl[-c(1)],
+Ageblk[-c(1)],Agetov[-c(1)])
+View(Agestat)
 
